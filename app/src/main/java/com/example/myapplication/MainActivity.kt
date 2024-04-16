5bd26cbc1aea00e290ef83f5b9e98ba1aaa5ead9
huangjingliang003 <huangjingliang003@realsee.com>
2024-04-16 19:35:34 +0800

    页面优化

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.myapplication.UrlDemo.UrlActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        findViewById<View>(R.id.button_jump).setOnClickListener {
+            testURL()
+//        }
+    }
+
+    private fun testURL() {
+        val intent = Intent(this@MainActivity, UrlActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+}
--- app/src/main/java/com/example/myapplication/UrlDemo/UrlActivity.kt
+++ app/src/main/java/com/example/myapplication/UrlDemo/UrlActivity.kt
@@ -1,23 +1,25 @@
 package com.example.myapplication.UrlDemo
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
-import kotlinx.android.synthetic.main.activity_url.webView
+import kotlinx.android.synthetic.main.activity_url.button_jump_url
+
 
 class UrlActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_url)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         val url = "realsee://wakeup?source=REALSEE_TECH&projectName=name&vr_type=phone&device_type=1&action_type=project_create&appid=124&callbal_url=ssssss";
         val uri = Uri.parse (url);
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        button_jump_url.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
